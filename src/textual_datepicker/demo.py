7deeab5107948003463968074fb1e19645c172fe
TomJGooding <101601846+TomJGooding@users.noreply.github.com>
2023-04-23 12:04:35 +0100

    feat: add dark mode toggle to demo

--- src/textual_datepicker/demo.py
+++ src/textual_datepicker/demo.py
@@ -1,4 +1,5 @@
 from textual.app import App, ComposeResult
+from textual.widgets import Footer
 
 from textual_datepicker.datepicker import DatePicker
 
@@ -9,9 +10,13 @@ class DatePickerDemo(App):
         align: center middle;
     }
     """
+    BINDINGS = [
+        ("ctrl+t", "app.toggle_dark", "Toggle Dark mode"),
+    ]
 
     def compose(self) -> ComposeResult:
         yield DatePicker()
+        yield Footer()
 
     def on_mount(self) -> None:
         self.query_one(DatePicker).focus()
